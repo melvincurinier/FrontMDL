d904c0bddfb565a95aab51a1746297f090aeb936
melvincurinier <77935728+melvincurinier@users.noreply.github.com>
2023-03-07 15:24:38 +0100

    fonction ajouterClient

--- index.js
+++ index.js
@@ -2,12 +2,29 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-function main(){
+let clients = [];
+
+function ajouterClient(_email, _prenom, _nom, _societe, _pays) {
+    var date = new Date();
+    let client = {
+        id: clients.length + 1,
+        email: _email,
+        first: _prenom,
+        last: _nom,
+        company: _societe,
+        country: _pays,
+        createdAt: date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear()
+    };
+    clients.push(client);
+}
+
+
+function main() {
     app.use(express.static("public"));
 
-    app.listen(port, function(){
+    app.listen(port, function () {
         console.log(`Example app listening on port ${port}`);
     });
 }
 
-main();
+ajouterClient('fdsfd', 'dsfdsf', 'dsfds', 'dsfds', 'dsfds');
