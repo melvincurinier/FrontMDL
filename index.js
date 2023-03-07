ce312f3023c58709fce2436d333cd561be3a2ea2
melvincurinier <77935728+melvincurinier@users.noreply.github.com>
2023-03-07 09:49:52 +0100

    initial commit

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 const express = require("express");
+const { sendFile } = require("express/lib/response");
 const app = express();
 const port = 3000;
 
 function main(){
-    app.get("/", (req, res) => {
-        const currentDate = new Date();
-        console.log("page called at " + currentDate);
-        res.send("Hello World ! Nous sommes le " + currentDate);
-    });
+    app.use(express.static("public"));
 
     app.listen(port, function(){
         console.log(`Example app listening on port ${port}`);
