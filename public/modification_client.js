b50f1137a08fa9f8f658813c1b975bbda06536b0
melvincurinier <77935728+melvincurinier@users.noreply.github.com>
2023-04-25 16:40:53 +0200

    Modification de la modification des clients

--- public/modification_client.js
+++ public/modification_client.js
@@ -1,4 +1,5 @@
 var idCustomer;
+const testEmail = /^[a-z0-9.-]{2,}@+[a-z0-9.-]{2,}$/i;
 
 function getData(id) {
     let url = "http://localhost:3001/api/customer?id=" + id;
@@ -41,26 +42,32 @@ function submitForm() {
     var company = document.getElementById("company").value;
     var country = document.getElementById("country").value;
 
-    var data = {
-        email: email,
-        first: first,
-        last: last,
-        company: company,
-        country: country
-    };
+    if (!email || !first || !last || !company || !country) {
+        alert("Informations manquantes !");
+    } else if (!testEmail.test(email)) {
+        alert("Mauvais format de l'email");
+    } else {
+        var data = {
+            email: email,
+            first: first,
+            last: last,
+            company: company,
+            country: country
+        };
 
-    fetch('http://localhost:3001/api/modifCustomer?id=' + idCustomer, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Réponse de l\'API:', data);
+        fetch('http://localhost:3001/api/modifCustomer?id=' + idCustomer, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
         })
-        .catch(error => {
-            console.error('Erreur:', error);
-        });
+            .then(response => response.json())
+            .then(data => {
+                console.log('Réponse de l\'API:', data);
+            })
+            .catch(error => {
+                console.error('Erreur:', error);
+            });
+    }
 }
